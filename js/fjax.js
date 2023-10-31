c40c7e22178fcf60b80b9c0425da96e79d10f77e
EyalPaz-700 <eyalp700@gmail.com>
2023-10-31 14:16:39 +0200

    intial fjax

---
+++ js/fjax.js
@@ -0,0 +1,23 @@
+class Fjax{
+    constructor(){
+        this._url = undefined
+        this._method = undefined
+        this._content = undefined
+        this._response = undefined
+        this._onload = undefined
+    }
+    open(url,method){
+        this._url = url
+        this._method = method
+    }
+    send(content = undefined){
+        if ( (!content && (this._method === "PUT" || this._method == "GET") ) || (content && (this._method === "POST" || this._method === "PUT" ))){
+            const packet = new Packet(this._url, this._method, content)
+            this._response = Network.sendRequest(packet)
+            this._onload.call(this)
+        }
+    }
+    set onload(f){
+        this._onload = f
+    }
+}
