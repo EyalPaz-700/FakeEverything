0daf6db62a5417091fdb922383ea9a85b8bce917
ori-eph <oriephrati@hilma.tech>
2023-11-01 11:38:56 +0200

    current user

--- js/app.js
+++ js/app.js
@@ -2,33 +2,38 @@ import { Fjax } from "./fjax.js";
 import { resetDB } from "./DB.js";
 
 const templates = {
-    homeTemplate : document.getElementById('home-temp'),
-    loginTemplate : document.getElementById('login-temp'),
-    plantTemplate : document.getElementById('plant-temp'),
-    profileTemplate : document.getElementById('my-profile-temp')
+    homeTemplate: document.getElementById('home-temp'),
+    loginTemplate: document.getElementById('login-temp'),
+    plantTemplate: document.getElementById('plant-temp'),
+    profileTemplate: document.getElementById('my-profile-temp')
 }
 
-function initApp(){
+function initApp() {
     document.body.appendChild(templates.loginTemplate.cloneNode(true).content)
-    history.pushState({page: "login"},"login", "#" + "login")
+    history.pushState({ page: "login" }, "login", "#" + "login")
     defineLoginOnClicks()
     window.onhashchange = () => {
-        movePage(history.state.page + "Template")}
-
+        movePage(history.state.page + "Template")
+    }
 }
 
-function defineLoginOnClicks(){
+function defineLoginOnClicks() {
     const loginBtn = document.getElementById('login-btn')
     loginBtn.onclick = () => {
+        localStorage.setItem("currentUser", JSON.stringify({
+            id: 1,
+            name: "daksjdlasd",
+            plants: [],
+        }))
         movePage("homeTemplate")
     }
 }
 
-function defineNavOnClicks(){
+function defineNavOnClicks() {
     const homeNav = document.getElementById('home-nav')
     const profileNav = document.getElementById('profile-nav')
     homeNav.onclick = () => {
-        
+
         movePage("homeTemplate")
     }
     profileNav.onclick = () => {
@@ -37,26 +42,41 @@ function defineNavOnClicks(){
 
 }
 
-function movePage(template){
+function movePage(template) {
     document.body.removeChild(document.body.children[9])
     document.body.appendChild(templates[template].cloneNode(true).content)
-    if (template === "loginTemplate"){
+    if (template === "loginTemplate") {
         defineLoginOnClicks()
     }
-    if (template === "homeTemplate"){
+    if (template === "homeTemplate") {
         fetchPlants(1)
         defineNavOnClicks()
 
     }
-    if (template === "profileTemplate"){
+    if (template === "profileTemplate") {
+        fetchUserPlants();
         defineNavOnClicks()
     }
     const name = template.split('T')[0]
-    history.pushState({page: name},name, "#" + name)
+    history.pushState({ page: name }, name, "#" + name)
 }
 
-function fetchPlants(pageNum){
+function fetchPlants(pageNum) {
     const plantContainer = document.getElementById('item-container')
+    plantContainer.appendChild(templates.plantTemplate.cloneNode(true).content)
+    plantContainer.appendChild(templates.plantTemplate.cloneNode(true).content)
+    plantContainer.appendChild(templates.plantTemplate.cloneNode(true).content)
+
+}
+
+function fetchUserPlants() {
+    const plantContainer = document.getElementById('item-container')
+    const reqUserPlants = new Fjax();
+    const userId = JSON.parse(localStorage.getItem("currentUser")).id;
+    reqUserPlants.open(`/api/users/${userId}`, "POST");
+    reqUserPlants.send({ prop: "plants" });
+    const userPlants = reqUserPlants._response._content;
+    console.log(userPlants);
     plantContainer.appendChild(templates.plantTemplate.cloneNode(true).content)
     plantContainer.appendChild(templates.plantTemplate.cloneNode(true).content)
     plantContainer.appendChild(templates.plantTemplate.cloneNode(true).content)
@@ -65,9 +85,5 @@ function fetchPlants(pageNum){
 
 initApp()
 resetDB()
-const a = new Fjax()
-a.open("/api/plants","POST")
-a.send({prop: "name", value: "d"})
-console.log(a)
 
 
